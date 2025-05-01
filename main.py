5093a4c39481a790c6f345bed9cc0b2f86e2ee02
github_ivorykeys87 <chase.r.catalano@gmail.com>
2025-05-01 13:57:49 -0400

    Cell added

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from graphics import *
 
 def main():
     win = Window(800, 600)
-    point1 = Point(5, 10)
-    point2 = Point(20,50)
-    line = Line(point1, point2)
-    win.draw_line(line,"black")
+    point1 = Point(100, 100)
+    point2 = Point(200,200)
+    cell= Cell(True, True, True, True, point1, point2, win)
+    cell.draw()
     win.wait_for_close()
 
 main()
