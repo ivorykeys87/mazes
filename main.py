0b5ec063eda352811ca9f95d0ef102018f7cb26b
github_ivorykeys87 <chase.r.catalano@gmail.com>
2025-05-01 22:15:43 -0400

    Initiate mazes

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 from graphics import *
 from cell import *
+from maze import *
 
 def main():
     win = Window(800, 600)
-    point1 = Point(100, 100)
-    point2 = Point(200,200)
-    point3 = Point(200, 100)
-    point4 = Point(300,200)
-    cell= Cell(p1=point1, p2=point2, window=win)
-    cell2 = Cell(p1=point3, p2=point4, window=win)
-    cell.draw()
-    cell2.draw()
-    cell.draw_move(cell2)
+    maze = Maze(10,10,5,5,50,50,win)
+    maze._create_cells()
     win.wait_for_close()
 
 main()
---
+++ maze.py
@@ -0,0 +1,40 @@
+from cell import Cell
+from graphics import Point, Window
+import time
+
+class Maze():
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        cx1 = self.x1
+        cy1 = self.y1
+        cx2 = self.x1 + self.cell_size_x
+        cy2 = self.y1 + self.cell_size_y
+        for c in range(self.num_cols):
+            row = []
+            for r in range(self.num_rows):
+                cell = Cell(p1=Point(cx1,cy1), p2 = Point(cx2,cy2), window=self.win)
+                row.append(cell)
+                cell.draw()
+                self.animate()
+                cy1 += self.cell_size_y
+                cy2 += self.cell_size_y
+            self.cells.append(row)
+            cx1 += self.cell_size_x
+            cy1 = self.y1
+            cx2 += self.cell_size_x
+            cy2 = self.y1 + self.cell_size_y
+
+    def animate(self):
+        self.win.redraw()
+        time.sleep(0.15)
+        
